008e29c7a17c1529f2f25f1f503f40593939086d
Mounic Madiraju <mounicraju@gmail.com>
2017-06-16 09:25:25 +0800

    Add files via upload

---
+++ method1.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Data;
+using System.Configuration;
+using System.Net;
+using System.Collections.Generic;
+
+namespace ContentExtractor
+// Method 1 //
+public void getRobots()
+{
+    WebClient wClient = new WebClient();
+    string url = String.Format("http://{0}/robots.txt", urlBox.Text);
+
+    try
+    {
+        Stream data = wClient.OpenRead(url);
+        StreamReader read = new StreamReader(data);
+        string[] lines = new string[] { read.ReadToEnd() };
+
+        foreach (string line in lines)
+        {
+            textBox1.AppendText(line + System.Environment.NewLine);
+        }
+    }
+    catch (WebException ex)
+    {
+        MessageBox.Show(ex.Message, null, MessageBoxButtons.OK);
+    }
+}
---
+++ method2.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Data;
+using System.Configuration;
+using System.Net;
+using System.Collections.Generic;
+
+namespace ContentExtractor
+
+// Method 2 //
+
+public void getRobots()
+{
+    WebClient wClient = new WebClient();
+    string robotText;
+    string[] robotLines;
+    System.Text.StringBuilder robotStringBuilder;
+
+    robotText = wClient.DownloadString(String.Format("http://{0}/robots.txt", urlBox.Text));
+
+    robotLines = robotText.Split(Environment.NewLine);
+
+    robotStringBuilder = New StringBuilder();
+
+    foreach (string line in robotLines)
+    {
+        robotStringBuilder.Append(line);
+        robotStringBuilder.Append(Environment.NewLine);
+    }
+
+    textbox1.Text = robotStringBuilder.ToString();
+}
