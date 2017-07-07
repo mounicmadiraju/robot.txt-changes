24497388528251812b6bb66441bb4aa04da67cc2
Mounic Madiraju <mounicraju@gmail.com>
2017-07-07 14:43:51 +0800

    Update method1.cs

--- method1.cs
+++ method1.cs
@@ -11,7 +11,7 @@ using System.Net;
 using System.Collections.Generic;
 
 namespace ContentExtractor
-// Method 1 starts from here //
+
 public void getRobots()
 {
     WebClient wClient = new WebClient();
