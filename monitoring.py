c9709caa858b7c952e98089428de45937d165118
Mounic Madiraju <mounicraju@gmail.com>
2017-07-07 14:45:28 +0800

    Create monitoring.py

---
+++ monitoring.py
@@ -0,0 +1,11 @@
+# Import requests (to download the page)
+import requests
+
+# Import BeautifulSoup (to parse what we download)
+from bs4 import BeautifulSoup
+
+# Import Time (to add a delay between the times the scape runs)
+import time
+
+# Import smtplib (to allow us to email)
+import smtplib
